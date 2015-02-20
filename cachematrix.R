cebc61f75cc573d654b91023da9428d17c79b5ba
marrycv <chrizzycv@Marry-Chriz-Cabauatan-Villanuevas-MacBook-Pro.local>
2015-02-20 03:45:27 +0100

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,11 @@
-## R function to compute the inverse of a square matrix and then CACHE it  
-## to potentially reduce computational time  
-         
+## "cachematrix.R": R function to compute the inverse of a square matrix and then CACHE it  
+##  to potentially reduce computational time  especially when input matrix is large and dense.
+##  How to use: 1.source the code, 2.input matrix, 3.call makeCacheMatrix, 4. call cacheSolve
+##  Try: mat<-matrix(c(1,2,3,4),nrow=2,ncol=2); x<-makeCacheMatrix(mat); sol1<-cacheSolve(x)
 
-## makeCacheMatrix: creates and returns a special matrix 
+## makeCacheMatrix: creates and returns a special matrix by setting and getting the matrix, and
+## setting and getting the inverse of the matrix
+
 makeCacheMatrix <- function(x=matrix()){
   m <- NULL
   set <- function(y) {
@@ -17,7 +20,10 @@ makeCacheMatrix <- function(x=matrix()){
        getinverse = getinverse)
 }
 
-# cacheSolve: Function to compute the inverse of the matrix 
+
+# cacheSolve: Function to compute the inverse of the special matrix created by makeCacheMatrix, 
+#             it calls the function "solve" 
+
 
 cacheSolve<-function(x, ...){
   m<-x$getinverse()
